72bd7739f8026900e972e0b0e3dde129c7a1e9fe
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-01-30 19:03:54 -0300

    Fix where clause

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ mod cli;
 mod storage;
 mod query;
 
+use query::parser::parse;
+
 fn main() {
-    query::parser::parse("select where name = Gabriel");    
+    parse("select where name = Gabriel and age > 18");    
+    parse("delete where name = Gabriel");    
+    parse("insert name = Gabriel, age = 18");    
 }
--- src/query/parser.rs
+++ src/query/parser.rs
@@ -1,4 +1,4 @@
-use pest::Parser;
+use pest::{Parser, iterators::Pair};
 
 #[derive(pest_derive::Parser)]
 #[grammar = "query/grammar/stupid-query-lang.pest"]
@@ -6,5 +6,27 @@ pub struct StupidQueryLangParser;
 
 pub fn parse(input: &str) {
     let pairs = StupidQueryLangParser::parse(Rule::stmt, input).unwrap();
-    println!("{:#?}", pairs);
+    for pair in pairs {
+        match pair.as_rule() {
+            Rule::EOI => break,
+            Rule::stmt => {
+                let action = pair.into_inner().next().unwrap().into_inner().next().unwrap();
+                perform_action(action);
+            },
+            _ => unreachable!(),
+        }
+    }
+}
+
+pub fn perform_action(action: Pair<Rule>) {
+    match action.as_rule() {
+        Rule::select => {
+            println!("Performing a select");
+            let where_subtree = action.into_inner().next().unwrap();
+            println!("{:#?}", where_subtree);
+        },
+        Rule::insert => println!("Performing a insert"),
+        Rule::delete => println!("Performing a delete"),
+        _ => unreachable!(), 
+    }
 }
