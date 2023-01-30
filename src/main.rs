51cdfa0b31138d75e1af4a86d60b9d1c75bbb1e4
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-01-30 15:37:16 -0300

    WIP improving stupid query language

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,10 @@
 #[macro_use] extern crate pest;
 
-use clap::Parser;
-
 mod model;
 mod cli;
 mod storage;
 mod query;
 
-use cli::{Cli,Command,RemoveParam};
-use storage::insert::create_person;
-use storage::select::{list,get_by_key};
-use storage::mutate::{remove_by_key,clear};
-
 fn main() {
-    let args = Cli::parse();
-    match args.command {
-        Command::Push { name, age } => {
-            let person = create_person(name, age); 
-            println!("Created person with name {}, age {}, and id {}", person.name, person.age, person.id);
-        },
-        Command::List {limit, min_age, max_age} => {
-            let result = list(
-                usize::from(limit), 
-                min_age, 
-                max_age
-            );
-            if result.len() == 0 {
-                println!("No record found");
-                return;
-            }
-            for person in result {
-                println!("{}: name: {}, age: {}", person.id, person.name, person.age);
-            }
-        },
-        Command::Remove { param } => {
-            match param {
-                RemoveParam::Id { val } => {
-                    remove_by_key(&val);
-                    println!("Removed user with id {val}");
-                },
-            }
-        },
-        Command::Get { key } => {
-            let person = get_by_key(&key).expect("Record not found with the given key");
-            println!("{}: name: {}, age: {}", person.id, person.name, person.age);
-        },
-        Command::Clear => {
-           clear(); 
-           println!("Removed all records");
-        },
-    }
+    query::parser::parse("select where name = Gabriel");    
 }
--- src/query/parser.rs
+++ src/query/parser.rs
@@ -1,3 +1,10 @@
+use pest::Parser;
+
 #[derive(pest_derive::Parser)]
 #[grammar = "query/grammar/stupid-query-lang.pest"]
 pub struct StupidQueryLangParser;
+
+pub fn parse(input: &str) {
+    let pairs = StupidQueryLangParser::parse(Rule::stmt, input).unwrap();
+    println!("{:#?}", pairs);
+}
