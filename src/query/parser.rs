247d15f48b0252d902534eef1649ad13910c1c7e
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2023-01-30 19:27:59 -0300

    WIP extracting where clause params

--- src/query/parser.rs
+++ src/query/parser.rs
@@ -1,4 +1,4 @@
-use pest::{Parser, iterators::Pair};
+use pest::{iterators::Pair, iterators::Pairs, Parser};
 
 #[derive(pest_derive::Parser)]
 #[grammar = "query/grammar/stupid-query-lang.pest"]
@@ -10,9 +10,15 @@ pub fn parse(input: &str) {
         match pair.as_rule() {
             Rule::EOI => break,
             Rule::stmt => {
-                let action = pair.into_inner().next().unwrap().into_inner().next().unwrap();
+                let action = pair
+                    .into_inner()
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .next()
+                    .unwrap();
                 perform_action(action);
-            },
+            }
             _ => unreachable!(),
         }
     }
@@ -22,11 +28,23 @@ pub fn perform_action(action: Pair<Rule>) {
     match action.as_rule() {
         Rule::select => {
             println!("Performing a select");
-            let where_subtree = action.into_inner().next().unwrap();
-            println!("{:#?}", where_subtree);
-        },
+            let conditions = action.into_inner().next().unwrap().into_inner();
+
+            let (min_age, max_age, exact_name) = get_where_params(conditions);
+        }
         Rule::insert => println!("Performing a insert"),
         Rule::delete => println!("Performing a delete"),
-        _ => unreachable!(), 
+        _ => unreachable!(),
     }
+}
+
+fn get_where_params(conditions: Pairs<Rule>) -> (Option<u32>, Option<u32>, Option<String>) {
+    for condition in conditions {
+        match condition.as_rule() {
+            Rule::condition => println!("Condition"),
+            Rule::operator => println!("Operator"),
+            _ => println!("Unknown"),
+        }
+    }
+    return (None, None, None);
 }
